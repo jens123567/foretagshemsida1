73b16af3e8066a457bdd5da3334bec83fda867a3
Jens Malmqvist <jens.malmqvist@learnet.se>
2020-11-13 15:46:52 +0100

    comit

--- index.js
+++ index.js
@@ -1,47 +1,32 @@
 let clicks = 0;
-var navbar = document.querySelector(".headerHide")
-var ham = document.querySelector(".hamButton")
+var navbar = document.querySelector(".headerHide");
+var ham = document.querySelector(".hamButton");
 
+function toggleHamburger() {
+  navbar.classList.toggle("headerShow");
+  console.log("yes");
+}
 
-function toggleHamburger(){
-    navbar.classList.toggle("headerShow")
-    console.log("yes")
-   /* ham.classList.toggle("showClose")*/
+function increaseCart() {
+  console.log("YEEET");
+  clicks++;
+  document.querySelector(".counter").innerHTML = clicks;
+}
+function decreseCart() {
+  clicks--;
+  if (clicks < 0) {
+    clicks = 0;
   }
-
-function increaseCart(){
-    console.log("YEEET")
-    clicks++;
-    document.querySelector("#counter").innerHTML = clicks;
-
-}
-function decreseCart(){
-    clicks--;
-    if (clicks < 0) {
-        clicks = 0;
-    }
-    document.querySelector("#counter").innerHTML = clicks == 0 ? "" : clicks;
-
-    
-
-}
-function rickRoll(){
-   
-        window.open( 
-            "https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
-    
+  document.querySelector(".counter").innerHTML = clicks == 0 ? "" : clicks;
 }
 
-ham.addEventListener("click", toggleHamburger)
-
-
-for(const x of [...document.querySelectorAll(".increaseCount")]){
-    x.addEventListener("click",increaseCart)
+for (const x of [...document.querySelectorAll(".hamButton")]) {
+  x.addEventListener("click", toggleHamburger);
 }
-for(const x of [...document.querySelectorAll(".decreaseCount")]){
-    x.addEventListener("click",decreseCart)
-    
+
+for (const x of [...document.querySelectorAll(".increaseCount")]) {
+  x.addEventListener("click", increaseCart);
 }
-for(const x of [...document.querySelectorAll(".ricked")]){
-    x.addEventListener("click", rickRoll)
+for (const x of [...document.querySelectorAll(".decreaseCount")]) {
+  x.addEventListener("click", decreseCart);
 }
